72e19db7741840c6e3fa9420a05fade799e9d408
jt146476 <54554424+jt146476@users.noreply.github.com>
2020-01-09 14:26:11 -0600

    update forms

--- AbandonedHouse/AbandonedHouse/DecidingToLeave.Designer.cs
+++ AbandonedHouse/AbandonedHouse/DecidingToLeave.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace AbondonedHouse_SearchingForm4
+﻿namespace AbandonedHouse
 {
-    partial class TalkOrLeave
+    partial class DecidingToLeave
     {
         /// <summary>
         /// Required designer variable.
@@ -28,41 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_Talk = new System.Windows.Forms.Button();
-            this.btn_Run = new System.Windows.Forms.Button();
             this.lbl_Footsteps = new System.Windows.Forms.Label();
+            this.btn_Run = new System.Windows.Forms.Button();
+            this.btn_Talk = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btn_Talk
-            // 
-            this.btn_Talk.Location = new System.Drawing.Point(189, 215);
-            this.btn_Talk.Name = "btn_Talk";
-            this.btn_Talk.Size = new System.Drawing.Size(113, 59);
-            this.btn_Talk.TabIndex = 0;
-            this.btn_Talk.Text = "Stay and see who the person is and talk to them. ";
-            this.btn_Talk.UseVisualStyleBackColor = true;
-            // 
-            // btn_Run
-            // 
-            this.btn_Run.Location = new System.Drawing.Point(486, 215);
-            this.btn_Run.Name = "btn_Run";
-            this.btn_Run.Size = new System.Drawing.Size(95, 62);
-            this.btn_Run.TabIndex = 1;
-            this.btn_Run.Text = "Leave and run home.";
-            this.btn_Run.UseVisualStyleBackColor = true;
             // 
             // lbl_Footsteps
             // 
             this.lbl_Footsteps.AutoSize = true;
-            this.lbl_Footsteps.Location = new System.Drawing.Point(222, 65);
+            this.lbl_Footsteps.Location = new System.Drawing.Point(173, 89);
             this.lbl_Footsteps.Name = "lbl_Footsteps";
             this.lbl_Footsteps.Size = new System.Drawing.Size(341, 39);
-            this.lbl_Footsteps.TabIndex = 2;
+            this.lbl_Footsteps.TabIndex = 5;
             this.lbl_Footsteps.Text = "You did the right thing, but while you\'re upstairs-you hear someone say,\r\n \"What " +
     "are you doing here?\" There footsteps are coming towards you. \r\nWhat is your next" +
     " move? ";
             // 
-            // TalkOrLeave
+            // btn_Run
+            // 
+            this.btn_Run.Location = new System.Drawing.Point(437, 239);
+            this.btn_Run.Name = "btn_Run";
+            this.btn_Run.Size = new System.Drawing.Size(95, 62);
+            this.btn_Run.TabIndex = 4;
+            this.btn_Run.Text = "Leave and run home.";
+            this.btn_Run.UseVisualStyleBackColor = true;
+            // 
+            // btn_Talk
+            // 
+            this.btn_Talk.Location = new System.Drawing.Point(140, 239);
+            this.btn_Talk.Name = "btn_Talk";
+            this.btn_Talk.Size = new System.Drawing.Size(113, 59);
+            this.btn_Talk.TabIndex = 3;
+            this.btn_Talk.Text = "Stay and see who the person is and talk to them. ";
+            this.btn_Talk.UseVisualStyleBackColor = true;
+            // 
+            // DecidingToLeave
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -70,8 +70,8 @@
             this.Controls.Add(this.lbl_Footsteps);
             this.Controls.Add(this.btn_Run);
             this.Controls.Add(this.btn_Talk);
-            this.Name = "TalkOrLeave";
-            this.Text = "TalkOrLeave";
+            this.Name = "DecidingToLeave";
+            this.Text = "DecidingToLeave";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,8 +79,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btn_Talk;
-        private System.Windows.Forms.Button btn_Run;
         private System.Windows.Forms.Label lbl_Footsteps;
+        private System.Windows.Forms.Button btn_Run;
+        private System.Windows.Forms.Button btn_Talk;
     }
 }
