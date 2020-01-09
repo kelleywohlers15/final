ea53a1009c8be8cc30d89b13066622b3ee5c1521
jt146476 <54554424+jt146476@users.noreply.github.com>
2020-01-09 14:58:21 -0600

    update stuff

---
+++ AbandonedHouse/AbandonedHouse/ExploringTogether.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbandonedHouse
+{
+    public partial class ExploringTogether : Form
+    {
+        public ExploringTogether()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Stay_Click(object sender, EventArgs e)
+        {
+            new LocationInHouse().Show();
+            this.Visible = false;
+        }
+    }
+}
